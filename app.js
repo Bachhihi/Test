a86e722f35418c2e9dc1edfd10aec366d20f8aa3
Bachhihi <80698524+Bachhihi@users.noreply.github.com>
2021-04-17 05:31:51 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+// A-2
+let a = "abc";
+let b = "1234";
+let result = "";
+if(a.length>b.length){
+    for(i=0;i<a.length;i++);
+    result += a[i]+b[i]
+}
+else{
+    for(i=0;i<a.length;i++);
+    result += a[i]+b[i]
+}
